87961a08f366de9639ccd8e4c57e840033ba7376
Martin Muzatko <martin@happy-css.com>
2017-03-05 21:27:43 +0100

    Added more speech recognition and synthesis options

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import './less/main.less'
 import './flex.scss'
 
-import './tags/app.html'
+import './src/tags/app.html'
 import 'riot'
 
 riot.mount('*')
---
+++ src/classes/speech-dialog.js
@@ -0,0 +1,11 @@
+export default class SpeechDialog {
+	constructor() {
+		{
+			'aktionen wohnzimmer' : {regexp: /a/, callback: ()=>{}},
+				'welche denn?'
+				'welche kann ich?'
+					'rot',
+					'blau'
+		}
+	}
+}
